ec023350673042e8d2f2a094d581f2a20a6a6c20
Rasmus Petersson <rasmus.petersson1@yaelev.se>
2021-10-21 12:28:57 +0200

    remove unused lines

--- src/Game.java
+++ src/Game.java
@@ -3,8 +3,6 @@ public class Game {
 
 	public static void main(String[] args) {
 		System.out.println("This is a game.");
-		System.out.println("This is a another line.");
-		System.out.println("This is a another another line.");
 		
 		System.out.println("This is Rasmus line");
 	}
