66a1863861e799cbf9c1743ad16fcfbc5e6e16b0
Rasmus Petersson <rasmus.petersson1@yaelev.se>
2021-10-25 13:50:18 +0200

    Add a new line (test)

--- src/GameBoard.java
+++ src/GameBoard.java
@@ -3,15 +3,14 @@ public class GameBoard {
 	
 	public static void gameBoard() {
 		
-System.out.println("     1   2   3\n");		
-System.out.println("1    " + "x" + " | " + "  | ");	
-System.out.println("    ---+---+---");
-System.out.println("2    " + "  | " + "x" + " | ");	
-System.out.println("    ---+---+---"); 
-System.out.println("3    " + "  | " + "  | " + "x");
-
-System.out.println("Hello!");
-
+		System.out.println("     1   2   3\n");		
+		System.out.println("1    " + "x" + " | " + "  | ");	
+		System.out.println("    ---+---+---");
+		System.out.println("2    " + "  | " + "x" + " | ");	
+		System.out.println("    ---+---+---"); 
+		System.out.println("3    " + "  | " + "  | " + "x");
+		System.out.println("Hello!");
+		System.out.println("Hej, det här är Rasmus");
 	}
 	
 }
