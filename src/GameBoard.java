bff36dc98f6978d2885bb145dc7765fb76e26d3e
lisahagberg <lisahagberg@192.168.3.9>
2021-10-25 13:46:20 +0200

    board //Lisa

--- src/GameBoard.java
+++ src/GameBoard.java
@@ -1,4 +1,15 @@
 
 public class GameBoard {
 	
+	public static void gameBoard() {
+		
+System.out.println("     1   2   3\n");		
+System.out.println("1    " + "x" + " | " + "  | ");	
+System.out.println("    ---+---+---");
+System.out.println("2    " + "  | " + "x" + " | ");	
+System.out.println("    ---+---+---"); 
+System.out.println("3    " + "  | " + "  | " + "x");
+
+	}
+	
 }
--- src/GameMenu.java
+++ src/GameMenu.java
@@ -4,5 +4,7 @@ public class GameMenu {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Tic-Tac-Toe");
 		System.out.println("Test");
+		
+		
 	}
 }
