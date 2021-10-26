cf76b6638bb50b7fa95c0880c97a6b2b1d94ffcf
Rasmus Petersson <rasmus.petersson1@yaelev.se>
2021-10-26 13:18:03 +0200

    Add userInput to ScoreBoard

--- src/Game.java
+++ src/Game.java
@@ -1,7 +1,63 @@
 
 public class Game {
 
-	public static void game() {
+	// public static void game() {
+	public static void main(String[] args) {
+		GameBoard.initializeNewGame();
 		GameBoard.showGameBoard();
+		
+		char gamerTag = 'X';
+
+		String userInput = "";
+		System.out.println("First select a Row and then Column");
+		System.out.println("Select row: 2");
+		System.out.println("Select column: 3");
+		userInput = "" + 2 + 3;
+		
+		if(GameBoard.slot[convertInputToInt(userInput)-1] != ' ') {
+			System.out.println("Sorry, slot is occupied");
+		} else {
+			System.out.println("Sucess, you added your gamertag " + gamerTag);
+		}
+		
+		
+		
+		int test1 = convertInputToInt(userInput);
+		int test2 = convertInputToInt(userInput) -1;
+		GameBoard.slot[convertInputToInt(userInput)-1] = gamerTag;
+		System.out.println("Test: " + test1);
+		System.out.println("Test: " + test2);
+		
+		
+		if(GameBoard.slot[convertInputToInt(userInput)-1] != ' ') {
+			System.out.println("Sorry, slot is occupied");
+		} else {
+			System.out.println("Sucess, you added your gamertag " + gamerTag);
+		}
+		
+		GameBoard.showGameBoard();
+		
 	}
+	
+	public static int convertInputToInt(String userInput){
+		int boardNumber = switch (userInput) {
+		case "11" -> 1;
+		case "12" -> 2;
+		case "13" -> 3;
+		
+		case "21" -> 4;
+		case "22" -> 5;
+		case "23" -> 6;
+		
+		case "31" -> 7;
+		case "32" -> 8;
+		case "33" -> 9;
+		default -> 100;
+		};
+		return boardNumber;
+	}
+	
+
+	
+	
 }
--- src/GameBoard.java
+++ src/GameBoard.java
@@ -1,13 +1,28 @@
 
 public class GameBoard {
 	
+	static char[] slot = new char[9];
+	
+	public static void initializeNewGame() {
+		
+		for (int i = 0; i < slot.length; i++) {
+			slot[i] = ' ';
+		}
+	}
+	
+	public static void AddToGameBoard(){
+		
+	}
+	
+
 	public static void showGameBoard(){
+		
 		System.out.println("     1   2   3\n");		
-		System.out.println("1    " + "x" + " | " + "  | ");	
+		System.out.println("1    " + slot[0] + " | " + slot[1] + " | " + slot[2]);	
 		System.out.println("    ---+---+---");
-		System.out.println("2    " + "  | " + "x" + " | ");	
+		System.out.println("2    " + slot[3] + " | " + slot[4] + " | " + slot[5]);	
 		System.out.println("    ---+---+---"); 
-		System.out.println("3    " + "  | " + "  | " + "x");
+		System.out.println("3    " + slot[6] + " | " + slot[7] + " | " + slot[8]);
 		
 	}
 	
