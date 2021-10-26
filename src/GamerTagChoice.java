5eeb8a2e79ca733fd6126553e3120cf2e95a0b06
lisahagberg <lisahagberg@192.168.3.9>
2021-10-26 14:35:12 +0200

    GamerTagChoice //Lisa

---
+++ src/GamerTagChoice.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class GamerTagChoice {
+	
+	public static void main(String[] args) {
+
+		System.out.println("Start the game by choosing if you want to play as Xs or Os.");
+		System.out.print("Write X or O to choose: ");
+		Scanner s = new Scanner(System.in);
+		String gamerChoice = s.nextLine();
+		gamerChoice = gamerChoice.toUpperCase();
+
+		while (!"X".equals(gamerChoice) && !"O".equals(gamerChoice)) {
+			System.out.println("Invalid choice!");
+			System.out.print("Write X or O to choose: ");
+			gamerChoice = s.nextLine();
+			gamerChoice = gamerChoice.toUpperCase();
+		}
+
+		s.close();
+
+		if (gamerChoice.equals("X")) {
+			char gamerTag = 'X';
+			char computerTag = 'O';
+			System.out.println("You will plays as X.");
+
+		} else if (gamerChoice.equals("O")) {
+			char gamerTag = 'O';
+			char computerTag = 'X';
+			System.out.println("You will plays as O.");
+
+		}
+	}
+}
+
