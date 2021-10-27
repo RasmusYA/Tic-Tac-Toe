d0d1a81f523a26b17bfefd6d40a5650924868ceb
Rasmus Petersson <rasmus.petersson1@yaelev.se>
2021-10-27 15:08:41 +0200

    Added comments to functions

--- src/Game.java
+++ src/Game.java
@@ -4,62 +4,69 @@ public class Game {
 
 	public static void startGame() {
 		Scanner scanner = new Scanner(System.in);
-		//Sätter spelarens och motståndarens Tag till X eller O
+		// Sätter spelarens och motståndarens Tag till X eller O
 		char playerTag = GamerTagChoice.ChoosePlayer(scanner);
 		char computerTag = ' ';
-		if(playerTag == 'X')
+		if (playerTag == 'X')
 			computerTag = 'O';
 		else {
 			computerTag = 'X';
 		}
 
-		
 		boolean slotIsFull = false; // Tillfällig variabel för test
 		boolean threeInRow = false; // Tillfällig variabel för test
-		
-		GameBoard.initializeNewGame(); //Återställer spelbräder, alla slots blir tomma
-		GameBoard.showGameBoard(); //Visar spelbrädet
+
+		GameBoard.initializeNewGame(); // Återställer spelbräder, alla slots blir tomma
+		GameBoard.showGameBoard(); // Visar spelbrädet
 		System.out.println("First select a Row and then Column");
-		
-		//TODO --> Om alla platser är upptagna eller någon har 3 i rad så sätt boolean till false;
-		while(!slotIsFull) {
-			boolean addSlot = false;	
+
+		// TODO --> Om alla platser är upptagna eller någon har 3 i rad så sätt boolean
+		// till false;
+		while (!slotIsFull) {
+			boolean addSlot = false;
 			boolean enemyAddSlot = false;
-			
-			
-			slotIsFull = GameBoard.checkIfSlotsFull();
-			if(slotIsFull)
+
+			// Stops game if all slots in board are full
+			if (GameBoard.checkIfSlotsFull())
 				break;
-			
-		while(!addSlot) {
-			try {
-				//addSlot retunerar sant eller falskt om där man vill placera sin spelpjäs är upptagen eller inte.
-				//Om platsen är tom retunerar den sant.
-				String userInput = selectSlot(scanner);
-				addSlot = GameBoard.AddToGameBoard(convertInputToInt(userInput), playerTag);
+
+			while (!addSlot) {
+				try {
+					// addSlot retunerar sant eller falskt om där man vill placera sin spelpjäs är
+					// upptagen eller inte.
+					// Om platsen är tom retunerar den sant.
+					String userInput = selectSlot(scanner);
+					addSlot = GameBoard.AddToGameBoard(convertInputToInt(userInput), playerTag);
+				}
+				// OM användaren matar in något annat än mellan 1-3
+				catch (ArrayIndexOutOfBoundsException ex) {
+					System.out.println("Sorry, slot does not exist.");
+				}
+
+				// Stops game if all slots in board are full
+				if (GameBoard.checkIfSlotsFull())
+					break;
+
+				// Enemys turn
+				while (!enemyAddSlot) {
+					enemyAddSlot = GameBoard.AddToGameBoard(Enemy.enemyTurn(), computerTag);
+				}
 			}
-			//OM användaren matar in något annat än mellan 1-3
-			catch(ArrayIndexOutOfBoundsException ex) {
-				System.out.println("Sorry, slot does not exist.");
-			}
-			
-			slotIsFull = GameBoard.checkIfSlotsFull();
-			if(slotIsFull)
-				break;
-			
-						
-			while(!enemyAddSlot) {
-				
-				//Enemnys turn
-				enemyAddSlot = GameBoard.AddToGameBoard(Enemy.enemyTurn(), computerTag);
-			}
+			GameBoard.showGameBoard();
 		}
-		GameBoard.showGameBoard();
-		}
+
+		
+		// Game Over
+		System.out.println("Game Over");
+
+		
+		// Present winner/looser/ draw or tie
+
 		
 	}
+
 	// Spelar får välja rad och kolumn vart de vill lägga sin nästa
-	//spelpjäs som retunerar en sträng med rad + column
+	// spelpjäs som retunerar en sträng med rad + column
 	public static String selectSlot(Scanner scanner) {
 		System.out.print("Select row: ");
 		String num1 = scanner.next();
@@ -69,8 +76,9 @@ public class Game {
 
 		return userInput;
 	}
-	//Konverterar det som användaren skriver, till exempel row: 1 och column: 3
-	//Input blir då 13, denna function konverterar detta till 2.
+
+	// Konverterar det som användaren skriver, till exempel row: 1 och column: 3
+	// Input blir då 13, denna function konverterar detta till 2.
 	public static int convertInputToInt(String userInput) {
 		int boardNumber = switch (userInput) {
 		case "11" -> 0;
