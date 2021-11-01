8483de0eb04a1282506c8307807557d594f6a458
OskarJansson92 <oskarj92@gmail.com>
2021-11-01 14:32:17 +0100

    "How to play"-option functionality

--- src/GameMenu.java
+++ src/GameMenu.java
@@ -1,5 +1,6 @@
 import java.io.*;
 import java.util.*;
+import java.util.concurrent.TimeUnit;
 
 public class GameMenu {
 
@@ -18,8 +19,9 @@ public class GameMenu {
 		+ " |Choose an Option:  |\n"
 		+ " |(1) -- Start Game  |\n"
 		+ " |(2) -- How to Play |\n"
-		+ " |(3) -- Credits     |\n"
-		+ " |(4) -- Exit        |\n"
+		+ " |(3) -- Rules       |\n"
+		+ " |(4) -- Credits     |\n"
+		+ " |(5) -- Exit        |\n"
 		+ " X-------------------O");
 		System.out.println (menu);
 		System.out.print(">");
@@ -30,6 +32,44 @@ public class GameMenu {
 			Game.startGame();
 			break;
 		case "2":
+			String howto1 = "The game board consists of a criss-cross of 3 horizontal rows and 3 vertical columns:"; 
+			for (int j = 0; j < howto1.length(); j++) {
+			      System.out.print(howto1.charAt(j));
+			      Thread.sleep(40);
+			}
+			TimeUnit.SECONDS.sleep(1);
+			System.out.println();
+			System.out.println("\n"
+			+ "         COLUMNS  \n"
+			+ "        1   2   3 \n"		
+			+ "     1    |   |   \n"	
+			+ "  R    ---+---+---\n"
+			+ "  O  2    |   |   \n"	
+			+ "  W    ---+---+---\n" 
+			+ "  S  3    |   |   \n");
+			System.out.println();
+			TimeUnit.SECONDS.sleep(2);
+			
+			String howto2 = "You place your mark by entering a number 1-3.  First for which row and then once more for choice of column.\n"
+					+ "Example: Player X presses '2' for row and '3' for column, resulting in this game state:";
+			for (int k = 0; k < howto2.length(); k++) {
+			      System.out.print(howto2.charAt(k));
+			      Thread.sleep(40);
+			}
+			TimeUnit.SECONDS.sleep(1);
+			System.out.println();
+			System.out.println("\n"
+			+ "        COLUMNS  \n"
+			+ "       1   2   3 \n"		
+			+ "    1    |   |   \n"	
+			+ " R    ---+---+---\n"
+			+ " O  2    |   | X  \n"	
+			+ " W    ---+---+---\n" 
+			+ " S  3    |   |   \n");
+			System.out.println();
+			break;
+			
+		case "3":
 			try {
 				var input = new BufferedReader (new FileReader("Rules.txt"));
 			
@@ -51,7 +91,7 @@ public class GameMenu {
 			s2.nextInt();
 				 break;
 			 
-		case "3":
+		case "4":
 			String title = "TIC-TAC-TOE";
 			for (int j = 0; j < welcome.length(); j++) {
 			      System.out.print(title.charAt(j));
@@ -64,7 +104,7 @@ public class GameMenu {
 					+"Victoria.......Lead Designer\n"
 					+"Oskar............UX Designer");
 			break;
-		case "4":
+		case "5":
 			System.out.println("Program shutdown. Goodbye!");
 			System.exit(0);
 			break;
