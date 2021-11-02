c627ee29df37dc9db4509a0a25e26b470100f856
lisahagberg <lisahagberg92@gmail.com>
2021-11-02 09:33:09 +0100

    minor fix

--- src/edu/grupp4b/enemy/EnemySmarter.java
+++ src/edu/grupp4b/enemy/EnemySmarter.java
@@ -7,177 +7,179 @@ public class EnemySmarter {
 	public static int blockPlayer(char gameTag) {
 		char playerTag = gameTag;
 		
-			if (GameBoard.slot[0] == playerTag && GameBoard.slot[1] == playerTag && GameBoard.slot[2] == ' ') {
-				System.out.println("\nMOOAHAHA, CONSIDER YOURSELF BLOCKED! I PICK SLOT 2");
-				return 2;
-			} else if (GameBoard.slot[0] == playerTag && GameBoard.slot[2] == playerTag && GameBoard.slot[1] == ' ') {
-				System.out.println("\nMOOAHAHA, CONSIDER YOURSELF BLOCKED! I PICK SLOT 1");
-				return 1;
-			} else if (GameBoard.slot[1] == playerTag && GameBoard.slot[2] == playerTag && GameBoard.slot[0] == ' ') {
-				System.out.println("\nMOOAHAHA, CONSIDER YOURSELF BLOCKED! I PICK SLOT 0");
-				return 0;
-			// enemy winning row 2
-			} else if (GameBoard.slot[3] == playerTag && GameBoard.slot[4] == playerTag && GameBoard.slot[5] == ' ') {
-				System.out.println("\nMOOAHAHA, CONSIDER YOURSELF BLOCKED!  PICK SLOT 5");
-				return 5;
-			} else if (GameBoard.slot[3] == playerTag && GameBoard.slot[5] == playerTag && GameBoard.slot[4] == ' ') {
-				System.out.println("\nMOOAHAHA, CONSIDER YOURSELF BLOCKED! I PICK SLOT 4");
-				return 4;
-			} else if (GameBoard.slot[4] == playerTag && GameBoard.slot[5] == playerTag && GameBoard.slot[3] == ' ') {
-				System.out.println("\nMOOAHAHA, CONSIDER YOURSELF BLOCKED! I PICK SLOT 3");
-				return 3;
-			// enemy winning row 3
-			} else if (GameBoard.slot[6] == playerTag && GameBoard.slot[7] == playerTag && GameBoard.slot[8] == ' ') {
-				System.out.println("\nMOOAHAHA, CONSIDER YOURSELF BLOCKED! I PICK SLOT 8");
-				return 8;
-			} else if (GameBoard.slot[6] == playerTag && GameBoard.slot[8] == playerTag && GameBoard.slot[7] == ' ') {
-				System.out.println("\nMOOAHAHA, CONSIDER YOURSELF BLOCKED! I PICK SLOT 7");
-				return 7;
-			} else if (GameBoard.slot[7] == playerTag && GameBoard.slot[8] == playerTag && GameBoard.slot[6] == ' ') {
-				System.out.println("\nMOOAHAHA, CONSIDER YOURSELF BLOCKED! I PICK SLOT 6");
-				return 6;
-				
-			// enemy winning column 1
-			} else if (GameBoard.slot[0] == playerTag && GameBoard.slot[3] == playerTag && GameBoard.slot[6] == ' ') {
-				System.out.println("\nMOOAHAHA, CONSIDER YOURSELF BLOCKED! I PICK SLOT 6");
-				return 6;
-			} else if (GameBoard.slot[0] == playerTag && GameBoard.slot[6] == playerTag && GameBoard.slot[3] == ' ') {
-				System.out.println("\nMOOAHAHA, CONSIDER YOURSELF BLOCKED! I PICK SLOT 3");
-				return 3;
-			} else if (GameBoard.slot[3] == playerTag && GameBoard.slot[6] == playerTag && GameBoard.slot[0] == ' ') {
-				System.out.println("\nMOOAHAHA, CONSIDER YOURSELF BLOCKED! I PICK SLOT 0");
-				return 0;
-		    // enemy winning column 2
-			} else if (GameBoard.slot[1] == playerTag && GameBoard.slot[4] == playerTag && GameBoard.slot[7] == ' ') {
-				System.out.println("\nMOOAHAHA, CONSIDER YOURSELF BLOCKED! I PICK SLOT 7");
-				return 7;
-			} else if (GameBoard.slot[1] == playerTag && GameBoard.slot[7] == playerTag && GameBoard.slot[4] == ' ') {
-				System.out.println("\nMOOAHAHA, CONSIDER YOURSELF BLOCKED! I PICK SLOT 4");
-				return 4;
-			} else if (GameBoard.slot[4] == playerTag && GameBoard.slot[7] == playerTag && GameBoard.slot[1] == ' ') {
-				System.out.println("\nMOOAHAHA, CONSIDER YOURSELF BLOCKED! I PICK SLOT 1");
-				return 1;
-		    // enemy winning column 3
-			} else if (GameBoard.slot[2] == playerTag && GameBoard.slot[5] == playerTag && GameBoard.slot[8] == ' ') {
-				System.out.println("\nMOOAHAHA, CONSIDER YOURSELF BLOCKED! I PICK SLOT 8");
-				return 8;
-			} else if (GameBoard.slot[2] == playerTag && GameBoard.slot[8] == playerTag && GameBoard.slot[5] == ' ') {
-				System.out.println("\nMOOAHAHA, CONSIDER YOURSELF BLOCKED!  PICK SLOT 5");
-				return 5;
-			} else if (GameBoard.slot[5] == playerTag && GameBoard.slot[8] == playerTag && GameBoard.slot[2] == ' ') {
-				System.out.println("\nMOOAHAHA, CONSIDER YOURSELF BLOCKED! I PICK SLOT 2");
-				return 2;
-		    	
-		    // enemy winning diagonally  1
-			} else if (GameBoard.slot[0] == playerTag && GameBoard.slot[4] == playerTag && GameBoard.slot[8] == ' ') {
-				System.out.println("\nMOOAHAHA, CONSIDER YOURSELF BLOCKED! I PICK SLOT 8");
-				return 8;
-			} else if (GameBoard.slot[0] == playerTag && GameBoard.slot[8] == playerTag && GameBoard.slot[4] == ' ') {
-				System.out.println("\nMOOAHAHA, CONSIDER YOURSELF BLOCKED! I PICK SLOT 4");
-				return 4;
-			} else if (GameBoard.slot[4] == playerTag && GameBoard.slot[8] == playerTag && GameBoard.slot[0] == ' ') {
-				System.out.println("\nMOOAHAHA, CONSIDER YOURSELF BLOCKED! I PICK SLOT 0");
-				return 0;
-		    // enemy winning diagonally  2
-			} else if (GameBoard.slot[2] == playerTag && GameBoard.slot[4] == playerTag && GameBoard.slot[6] == ' ') {
-				System.out.println("\nMOOAHAHA, CONSIDER YOURSELF BLOCKED! I PICK SLOT 6");
-				return 6;
-			} else if (GameBoard.slot[2] == playerTag && GameBoard.slot[6] == playerTag && GameBoard.slot[4] == ' ') {
-				System.out.println("\nMOOAHAHA, CONSIDER YOURSELF BLOCKED! I PICK SLOT 4");
-				return 4;
-			} else if (GameBoard.slot[4] == playerTag && GameBoard.slot[6] == playerTag && GameBoard.slot[2] == ' ') {
-				System.out.println("\nMOOAHAHA, CONSIDER YOURSELF BLOCKED! I PICK SLOT 2");
-				return 2;
-			} else {
+		// enemy block row 1
+		if (GameBoard.slot[0] == playerTag && GameBoard.slot[1] == playerTag && GameBoard.slot[2] == ' ') {
+			System.out.println("\nMOOAHAHA, CONSIDER YOURSELF BLOCKED! I PICK SLOT 2");
+			return 2;
+		} else if (GameBoard.slot[0] == playerTag && GameBoard.slot[2] == playerTag && GameBoard.slot[1] == ' ') {
+			System.out.println("\nMOOAHAHA, CONSIDER YOURSELF BLOCKED! I PICK SLOT 1");
+			return 1;
+		} else if (GameBoard.slot[1] == playerTag && GameBoard.slot[2] == playerTag && GameBoard.slot[0] == ' ') {
+			System.out.println("\nMOOAHAHA, CONSIDER YOURSELF BLOCKED! I PICK SLOT 0");
+			return 0;
+			// enemy block row 2
+		} else if (GameBoard.slot[3] == playerTag && GameBoard.slot[4] == playerTag && GameBoard.slot[5] == ' ') {
+			System.out.println("\nMOOAHAHA, CONSIDER YOURSELF BLOCKED!  PICK SLOT 5");
+			return 5;
+		} else if (GameBoard.slot[3] == playerTag && GameBoard.slot[5] == playerTag && GameBoard.slot[4] == ' ') {
+			System.out.println("\nMOOAHAHA, CONSIDER YOURSELF BLOCKED! I PICK SLOT 4");
+			return 4;
+		} else if (GameBoard.slot[4] == playerTag && GameBoard.slot[5] == playerTag && GameBoard.slot[3] == ' ') {
+			System.out.println("\nMOOAHAHA, CONSIDER YOURSELF BLOCKED! I PICK SLOT 3");
+			return 3;
+			// enemy block row 3
+		} else if (GameBoard.slot[6] == playerTag && GameBoard.slot[7] == playerTag && GameBoard.slot[8] == ' ') {
+			System.out.println("\nMOOAHAHA, CONSIDER YOURSELF BLOCKED! I PICK SLOT 8");
+			return 8;
+		} else if (GameBoard.slot[6] == playerTag && GameBoard.slot[8] == playerTag && GameBoard.slot[7] == ' ') {
+			System.out.println("\nMOOAHAHA, CONSIDER YOURSELF BLOCKED! I PICK SLOT 7");
+			return 7;
+		} else if (GameBoard.slot[7] == playerTag && GameBoard.slot[8] == playerTag && GameBoard.slot[6] == ' ') {
+			System.out.println("\nMOOAHAHA, CONSIDER YOURSELF BLOCKED! I PICK SLOT 6");
+			return 6;
+
+			// enemy block column 1
+		} else if (GameBoard.slot[0] == playerTag && GameBoard.slot[3] == playerTag && GameBoard.slot[6] == ' ') {
+			System.out.println("\nMOOAHAHA, CONSIDER YOURSELF BLOCKED! I PICK SLOT 6");
+			return 6;
+		} else if (GameBoard.slot[0] == playerTag && GameBoard.slot[6] == playerTag && GameBoard.slot[3] == ' ') {
+			System.out.println("\nMOOAHAHA, CONSIDER YOURSELF BLOCKED! I PICK SLOT 3");
+			return 3;
+		} else if (GameBoard.slot[3] == playerTag && GameBoard.slot[6] == playerTag && GameBoard.slot[0] == ' ') {
+			System.out.println("\nMOOAHAHA, CONSIDER YOURSELF BLOCKED! I PICK SLOT 0");
+			return 0;
+			// enemy block column 2
+		} else if (GameBoard.slot[1] == playerTag && GameBoard.slot[4] == playerTag && GameBoard.slot[7] == ' ') {
+			System.out.println("\nMOOAHAHA, CONSIDER YOURSELF BLOCKED! I PICK SLOT 7");
+			return 7;
+		} else if (GameBoard.slot[1] == playerTag && GameBoard.slot[7] == playerTag && GameBoard.slot[4] == ' ') {
+			System.out.println("\nMOOAHAHA, CONSIDER YOURSELF BLOCKED! I PICK SLOT 4");
+			return 4;
+		} else if (GameBoard.slot[4] == playerTag && GameBoard.slot[7] == playerTag && GameBoard.slot[1] == ' ') {
+			System.out.println("\nMOOAHAHA, CONSIDER YOURSELF BLOCKED! I PICK SLOT 1");
+			return 1;
+			// enemy block column 3
+		} else if (GameBoard.slot[2] == playerTag && GameBoard.slot[5] == playerTag && GameBoard.slot[8] == ' ') {
+			System.out.println("\nMOOAHAHA, CONSIDER YOURSELF BLOCKED! I PICK SLOT 8");
+			return 8;
+		} else if (GameBoard.slot[2] == playerTag && GameBoard.slot[8] == playerTag && GameBoard.slot[5] == ' ') {
+			System.out.println("\nMOOAHAHA, CONSIDER YOURSELF BLOCKED!  PICK SLOT 5");
+			return 5;
+		} else if (GameBoard.slot[5] == playerTag && GameBoard.slot[8] == playerTag && GameBoard.slot[2] == ' ') {
+			System.out.println("\nMOOAHAHA, CONSIDER YOURSELF BLOCKED! I PICK SLOT 2");
+			return 2;
+
+			// enemy block diagonally 1
+		} else if (GameBoard.slot[0] == playerTag && GameBoard.slot[4] == playerTag && GameBoard.slot[8] == ' ') {
+			System.out.println("\nMOOAHAHA, CONSIDER YOURSELF BLOCKED! I PICK SLOT 8");
+			return 8;
+		} else if (GameBoard.slot[0] == playerTag && GameBoard.slot[8] == playerTag && GameBoard.slot[4] == ' ') {
+			System.out.println("\nMOOAHAHA, CONSIDER YOURSELF BLOCKED! I PICK SLOT 4");
+			return 4;
+		} else if (GameBoard.slot[4] == playerTag && GameBoard.slot[8] == playerTag && GameBoard.slot[0] == ' ') {
+			System.out.println("\nMOOAHAHA, CONSIDER YOURSELF BLOCKED! I PICK SLOT 0");
+			return 0;
+			// enemy block diagonally 2
+		} else if (GameBoard.slot[2] == playerTag && GameBoard.slot[4] == playerTag && GameBoard.slot[6] == ' ') {
+			System.out.println("\nMOOAHAHA, CONSIDER YOURSELF BLOCKED! I PICK SLOT 6");
+			return 6;
+		} else if (GameBoard.slot[2] == playerTag && GameBoard.slot[6] == playerTag && GameBoard.slot[4] == ' ') {
+			System.out.println("\nMOOAHAHA, CONSIDER YOURSELF BLOCKED! I PICK SLOT 4");
+			return 4;
+		} else if (GameBoard.slot[4] == playerTag && GameBoard.slot[6] == playerTag && GameBoard.slot[2] == ' ') {
+			System.out.println("\nMOOAHAHA, CONSIDER YOURSELF BLOCKED! I PICK SLOT 2");
+			return 2;
+		} else {
 			return Enemy.enemyTurn();
 		}
 	}
-	
+
 	public static int EnemyGoForWin(char gameTag) {
 		char enemyTag = gameTag;
 		
-			if (GameBoard.slot[0] == enemyTag && GameBoard.slot[1] == enemyTag && GameBoard.slot[2] == ' ') {
-				System.out.println("\nI'M UNBEATABLE! I PICK SLOT 2");
-				return 2;
-			} else if (GameBoard.slot[0] == enemyTag && GameBoard.slot[2] == enemyTag && GameBoard.slot[1] == ' ') {
-				System.out.println("\nI'M UNBEATABLE! I PICK SLOT 1");
-				return 1;
-			} else if (GameBoard.slot[1] == enemyTag && GameBoard.slot[2] == enemyTag && GameBoard.slot[0] == ' ') {
-				System.out.println("\nI'M UNBEATABLE! I PICK SLOT 0");
-				return 0;
+		// enemy winning row 1
+		if (GameBoard.slot[0] == enemyTag && GameBoard.slot[1] == enemyTag && GameBoard.slot[2] == ' ') {
+			System.out.println("\nI'M UNBEATABLE! I PICK SLOT 2");
+			return 2;
+		} else if (GameBoard.slot[0] == enemyTag && GameBoard.slot[2] == enemyTag && GameBoard.slot[1] == ' ') {
+			System.out.println("\nI'M UNBEATABLE! I PICK SLOT 1");
+			return 1;
+		} else if (GameBoard.slot[1] == enemyTag && GameBoard.slot[2] == enemyTag && GameBoard.slot[0] == ' ') {
+			System.out.println("\nI'M UNBEATABLE! I PICK SLOT 0");
+			return 0;
 			// enemy winning row 2
-			} else if (GameBoard.slot[3] == enemyTag && GameBoard.slot[4] == enemyTag && GameBoard.slot[5] == ' ') {
-				System.out.println("\nI'M UNBEATABLE!  PICK SLOT 5");
-				return 5;
-			} else if (GameBoard.slot[3] == enemyTag && GameBoard.slot[5] == enemyTag && GameBoard.slot[4] == ' ') {
-				System.out.println("\nI'M UNBEATABLE! I PICK SLOT 4");
-				return 4;
-			} else if (GameBoard.slot[4] == enemyTag && GameBoard.slot[5] == enemyTag && GameBoard.slot[3] == ' ') {
-				System.out.println("\nI'M UNBEATABLE! I PICK SLOT 3");
-				return 3;
+		} else if (GameBoard.slot[3] == enemyTag && GameBoard.slot[4] == enemyTag && GameBoard.slot[5] == ' ') {
+			System.out.println("\nI'M UNBEATABLE!  PICK SLOT 5");
+			return 5;
+		} else if (GameBoard.slot[3] == enemyTag && GameBoard.slot[5] == enemyTag && GameBoard.slot[4] == ' ') {
+			System.out.println("\nI'M UNBEATABLE! I PICK SLOT 4");
+			return 4;
+		} else if (GameBoard.slot[4] == enemyTag && GameBoard.slot[5] == enemyTag && GameBoard.slot[3] == ' ') {
+			System.out.println("\nI'M UNBEATABLE! I PICK SLOT 3");
+			return 3;
 			// enemy winning row 3
-			} else if (GameBoard.slot[6] == enemyTag && GameBoard.slot[7] == enemyTag && GameBoard.slot[8] == ' ') {
-				System.out.println("\nMOOAHAHA, CONSIDER YOURSELF BLOCKED! I PICK SLOT 8");
-				return 8;
-			} else if (GameBoard.slot[6] == enemyTag && GameBoard.slot[8] == enemyTag && GameBoard.slot[7] == ' ') {
-				System.out.println("\nI'M UNBEATABLE! I PICK SLOT 7");
-				return 7;
-			} else if (GameBoard.slot[7] == enemyTag && GameBoard.slot[8] == enemyTag && GameBoard.slot[6] == ' ') {
-				System.out.println("\nI'M UNBEATABLE! I PICK SLOT 6");
-				return 6;
-				
+		} else if (GameBoard.slot[6] == enemyTag && GameBoard.slot[7] == enemyTag && GameBoard.slot[8] == ' ') {
+			System.out.println("\nMOOAHAHA, CONSIDER YOURSELF BLOCKED! I PICK SLOT 8");
+			return 8;
+		} else if (GameBoard.slot[6] == enemyTag && GameBoard.slot[8] == enemyTag && GameBoard.slot[7] == ' ') {
+			System.out.println("\nI'M UNBEATABLE! I PICK SLOT 7");
+			return 7;
+		} else if (GameBoard.slot[7] == enemyTag && GameBoard.slot[8] == enemyTag && GameBoard.slot[6] == ' ') {
+			System.out.println("\nI'M UNBEATABLE! I PICK SLOT 6");
+			return 6;
+
 			// enemy winning column 1
-			} else if (GameBoard.slot[0] == enemyTag && GameBoard.slot[3] == enemyTag && GameBoard.slot[6] == ' ') {
-				System.out.println("\nI'M UNBEATABLE! I PICK SLOT 6");
-				return 6;
-			} else if (GameBoard.slot[0] == enemyTag && GameBoard.slot[6] == enemyTag && GameBoard.slot[3] == ' ') {
-				System.out.println("\nI'M UNBEATABLE! I PICK SLOT 3");
-				return 3;
-			} else if (GameBoard.slot[3] == enemyTag && GameBoard.slot[6] == enemyTag && GameBoard.slot[0] == ' ') {
-				System.out.println("\nI'M UNBEATABLE! I PICK SLOT 0");
-				return 0;
-		    // enemy winning column 2
-			} else if (GameBoard.slot[1] == enemyTag && GameBoard.slot[4] == enemyTag && GameBoard.slot[7] == ' ') {
-				System.out.println("\nI'M UNBEATABLE! I PICK SLOT 7");
-				return 7;
-			} else if (GameBoard.slot[1] == enemyTag && GameBoard.slot[7] == enemyTag && GameBoard.slot[4] == ' ') {
-				System.out.println("\nI'M UNBEATABLE! I PICK SLOT 4");
-				return 4;
-			} else if (GameBoard.slot[4] == enemyTag && GameBoard.slot[7] == enemyTag && GameBoard.slot[1] == ' ') {
-				System.out.println("\nI'M UNBEATABLE! I PICK SLOT 1");
-				return 1;
-		    // enemy winning column 3
-			} else if (GameBoard.slot[2] == enemyTag && GameBoard.slot[5] == enemyTag && GameBoard.slot[8] == ' ') {
-				System.out.println("\nI'M UNBEATABLE! I PICK SLOT 8");
-				return 8;
-			} else if (GameBoard.slot[2] == enemyTag && GameBoard.slot[8] == enemyTag && GameBoard.slot[5] == ' ') {
-				System.out.println("\nI'M UNBEATABLE!  PICK SLOT 5");
-				return 5;
-			} else if (GameBoard.slot[5] == enemyTag && GameBoard.slot[8] == enemyTag && GameBoard.slot[2] == ' ') {
-				System.out.println("\nI'M UNBEATABLE! I PICK SLOT 2");
-				return 2;
-		    	
-		    // enemy winning diagonally  1
-			} else if (GameBoard.slot[0] == enemyTag && GameBoard.slot[4] == enemyTag && GameBoard.slot[8] == ' ') {
-				System.out.println("\nI'M UNBEATABLE! I PICK SLOT 8");
-				return 8;
-			} else if (GameBoard.slot[0] == enemyTag && GameBoard.slot[8] == enemyTag && GameBoard.slot[4] == ' ') {
-				System.out.println("\nI'M UNBEATABLE!! I PICK SLOT 4");
-				return 4;
-			} else if (GameBoard.slot[4] == enemyTag && GameBoard.slot[8] == enemyTag && GameBoard.slot[0] == ' ') {
-				System.out.println("\nI'M UNBEATABLE! I PICK SLOT 0");
-				return 0;
-		    // enemy winning diagonally  2
-			} else if (GameBoard.slot[2] == enemyTag && GameBoard.slot[4] == enemyTag && GameBoard.slot[6] == ' ') {
-				System.out.println("\nI'M UNBEATABLE! I PICK SLOT 6");
-				return 6;
-			} else if (GameBoard.slot[2] == enemyTag && GameBoard.slot[6] == enemyTag && GameBoard.slot[4] == ' ') {
-				System.out.println("\nI'M UNBEATABLE! I PICK SLOT 4");
-				return 4;
-			} else if (GameBoard.slot[4] == enemyTag && GameBoard.slot[6] == enemyTag && GameBoard.slot[2] == ' ') {
-				System.out.println("\nI'M UNBEATABLE! I PICK SLOT 2");
-				return 2;
-			} else {
+		} else if (GameBoard.slot[0] == enemyTag && GameBoard.slot[3] == enemyTag && GameBoard.slot[6] == ' ') {
+			System.out.println("\nI'M UNBEATABLE! I PICK SLOT 6");
+			return 6;
+		} else if (GameBoard.slot[0] == enemyTag && GameBoard.slot[6] == enemyTag && GameBoard.slot[3] == ' ') {
+			System.out.println("\nI'M UNBEATABLE! I PICK SLOT 3");
+			return 3;
+		} else if (GameBoard.slot[3] == enemyTag && GameBoard.slot[6] == enemyTag && GameBoard.slot[0] == ' ') {
+			System.out.println("\nI'M UNBEATABLE! I PICK SLOT 0");
+			return 0;
+			// enemy winning column 2
+		} else if (GameBoard.slot[1] == enemyTag && GameBoard.slot[4] == enemyTag && GameBoard.slot[7] == ' ') {
+			System.out.println("\nI'M UNBEATABLE! I PICK SLOT 7");
+			return 7;
+		} else if (GameBoard.slot[1] == enemyTag && GameBoard.slot[7] == enemyTag && GameBoard.slot[4] == ' ') {
+			System.out.println("\nI'M UNBEATABLE! I PICK SLOT 4");
+			return 4;
+		} else if (GameBoard.slot[4] == enemyTag && GameBoard.slot[7] == enemyTag && GameBoard.slot[1] == ' ') {
+			System.out.println("\nI'M UNBEATABLE! I PICK SLOT 1");
+			return 1;
+			// enemy winning column 3
+		} else if (GameBoard.slot[2] == enemyTag && GameBoard.slot[5] == enemyTag && GameBoard.slot[8] == ' ') {
+			System.out.println("\nI'M UNBEATABLE! I PICK SLOT 8");
+			return 8;
+		} else if (GameBoard.slot[2] == enemyTag && GameBoard.slot[8] == enemyTag && GameBoard.slot[5] == ' ') {
+			System.out.println("\nI'M UNBEATABLE!  PICK SLOT 5");
+			return 5;
+		} else if (GameBoard.slot[5] == enemyTag && GameBoard.slot[8] == enemyTag && GameBoard.slot[2] == ' ') {
+			System.out.println("\nI'M UNBEATABLE! I PICK SLOT 2");
+			return 2;
+
+			// enemy winning diagonally 1
+		} else if (GameBoard.slot[0] == enemyTag && GameBoard.slot[4] == enemyTag && GameBoard.slot[8] == ' ') {
+			System.out.println("\nI'M UNBEATABLE! I PICK SLOT 8");
+			return 8;
+		} else if (GameBoard.slot[0] == enemyTag && GameBoard.slot[8] == enemyTag && GameBoard.slot[4] == ' ') {
+			System.out.println("\nI'M UNBEATABLE!! I PICK SLOT 4");
+			return 4;
+		} else if (GameBoard.slot[4] == enemyTag && GameBoard.slot[8] == enemyTag && GameBoard.slot[0] == ' ') {
+			System.out.println("\nI'M UNBEATABLE! I PICK SLOT 0");
+			return 0;
+			// enemy winning diagonally 2
+		} else if (GameBoard.slot[2] == enemyTag && GameBoard.slot[4] == enemyTag && GameBoard.slot[6] == ' ') {
+			System.out.println("\nI'M UNBEATABLE! I PICK SLOT 6");
+			return 6;
+		} else if (GameBoard.slot[2] == enemyTag && GameBoard.slot[6] == enemyTag && GameBoard.slot[4] == ' ') {
+			System.out.println("\nI'M UNBEATABLE! I PICK SLOT 4");
+			return 4;
+		} else if (GameBoard.slot[4] == enemyTag && GameBoard.slot[6] == enemyTag && GameBoard.slot[2] == ' ') {
+			System.out.println("\nI'M UNBEATABLE! I PICK SLOT 2");
+			return 2;
+		} else {
 			return Enemy.enemyTurn();
 		}
 	}
